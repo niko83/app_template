8a0ed5ec42bed91ea8b7e7e15b7312895cfa96fd
m_nikolayenko <m_nikolayenko@wargaming.net>
2014-04-25 14:06:19 +0300

    add parse args

--- console.py
+++ console.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
+import sys
 
 from app import settings
 from app.db.postgresql import settings as postgresql_settings
-import ipdb; ipdb.set_trace()
 
+from argparse import ArgumentParser
+
+arguments = ArgumentParser(
+    prog='prog',
+    description='desc',
+    epilog='info')
+
+arguments.add_argument(
+    '-t', '--test_command',
+    action='test',
+    version='Test command'
+)
+
+console = arguments.parse_args(sys.argv[1:])
+
+print console.test
