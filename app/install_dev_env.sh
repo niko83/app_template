6f827220a38e9c062e94642c216970a629e3d9c0
m_nikolayenko <m_nikolayenko@wargaming.net>
2014-07-19 10:22:30 +0300

    update ssh

--- app/install_dev_env.sh
+++ app/install_dev_env.sh
@@ -18,8 +18,8 @@ git config --global push.default simple
 git config --global diff.tool vimdiff
 git config --global difftool.prompt false
 git config --global alias.d difftool
-git config --global user.email "ktotutus@gmail.com"
-git config --global user.name "niko"
+git config --global user.email "m_nikolayenko@wargaming.net"
+git config --global user.name "m_nikolayenko"
 git config --global core.editor vim
 
 sudo apt-get install exuberant-ctags nodejs
@@ -31,7 +31,7 @@ sudo mkdir /var/www
 sudo chown niko:niko -R /var/www/
 
 
-VIM
+#VIM
 #source ~/.vim/rc.vim
 #
 #" Fast scrool
@@ -44,23 +44,23 @@ VIM
 #nnoremap <Leader>sl :SLoad last.vim<CR>
 #
 #let g:pymode_lint_ignore = "E501,C0110,C0111"
-#let g:pymode_lint_ignore = "E501,C0110,C0111,W191,W0312,E711,E126,W0403,E126"
+#let g:pymode_lint_ignore = "E501,C0110,C0111,W191,W0312,E711,E126,W0403,E126,D101,D102,D103,D100"
 #let g:pymode_lint_checker = "pylint,pep8,pyflakes,mccabe"
 #" ~/.vim/bundle/python-mode/pymode/libs/pylama/lint/pylama_pylint/pylint.rc
 #" [FORMAT]
 #" max-line-length=99
 #" [MASTER]
-#" init-hook='import sys; sys.path.append("/home/niko/testproject/");sys.path.append("/var/www/test_sp/");' 
+#" init-hook='import sys; sys.path.append("/home/niko/testproject/");sys.path.append("/var/www/test_sp/");'
 #
 #au BufNewFile,BufRead *.py setl colorcolumn=80,99
 #
-#call jsmode#Default("g:jsmode_largefile", 10)
+#" call jsmode#Default("g:jsmode_largefile", 10)
 #
 #set tpm=100
 #
 #" ~/.vim/bundle/python-mode.git/pylibs/ropevim.py
-#" 342             if isinstance(ci['info'], unicode):
-#" 343                 ci['info'] = repr(ci['info'])
+#" 342 if isinstance(ci['info'], unicode):
+#" 343 ci['info'] = repr(ci['info'])
 
 
 
@@ -101,6 +101,8 @@ character-set-server = utf8
 collation-server = utf8_general_ci
 transaction-isolation = READ-COMMITTED
 default-time-zone = '+00:00'
+
+export HISTSIZE=10000
 
 USEFUL
 ======
